eeebf57227343a4a7942105f4c48cd9081f18cf4
CGermanSoto <c.germansoto@gmail.com>
2022-10-18 08:48:18 -0300

    Se agrega componentes y estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Profile from './Components/Profile';
+import DinamicWindow from './Components/DinamicWindow';
+
+
 function App() {
   return (
     <div className="App">
       <div className='body-container'>
         <Profile />
+        <DinamicWindow />
       </div>
       <div className='footer-container'>
 
---
+++ src/Components/DinamicWindow.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../Styles/DinamicWindow.css'
+
+function Window(){
+    return (
+        <div className="window-container">
+            Hola
+        </div>
+    );
+}
+
+export default Window;
