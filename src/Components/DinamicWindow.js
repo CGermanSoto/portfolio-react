a75a5c3cfdaff8eab23409fe4d682480c034bcd2
CGermanSoto <cgermansoto@gmail.com>
2022-10-24 09:32:46 -0300

    Commit transitorio

--- src/Components/DinamicWindow.js
+++ src/Components/DinamicWindow.js
@@ -4,8 +4,10 @@ import '../Styles/DinamicWindow.css'
 function Window(){
     return (
         <div className="window-container">
-            <h1 className="window-title">Hola, soy Ger</h1>
-            <p className="window-content">Backend Developer</p>
+            <div className="window-title-content-container">
+                <h1 className="window-title">Hola, soy Ger</h1>
+                <p className="window-content">Backend Developer</p>
+            </div>
             <div className="window-menu">
                 <button>Inicio</button>
                 <button>Ger</button>
---
+++ src/Components/FooterButton.js
@@ -0,0 +1,22 @@
+import React from "react";
+import '../Styles/FooterButton.css'
+import ButtonSocialMedia from "./subComponents/ButtonSocialMedia";
+
+function Footer(){
+    return (
+        <>
+            <ButtonSocialMedia 
+                imagen='github' />
+            <ButtonSocialMedia 
+                imagen='github' />
+            <ButtonSocialMedia 
+                imagen='github' />
+            <ButtonSocialMedia 
+                imagen='github' />
+            <ButtonSocialMedia 
+                imagen='github' />
+        </>
+    );
+}
+
+export default Footer;
