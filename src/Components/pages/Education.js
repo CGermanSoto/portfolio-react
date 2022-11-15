7e41ad8a0352212f3f274d83416bb78be9c91282
CGermanSoto <cgermansoto@gmail.com>
2022-11-15 09:27:00 -0300

    Se agregan campos en education.js

--- src/Components/pages/Education.js
+++ src/Components/pages/Education.js
@@ -6,28 +6,82 @@ function Education(){
         <div className="aboutMe-container">
             <h1 className="window-title p">Mis <span className="red mi">estudios</span></h1>
             <div className="parrafoAboutMe">
-
                 <div className="parrafo">
-                    <h1 className="p-titulo">NTT DATA Europe & LATAM </h1>
-                    
-                </div>
-                
-                <div className="parrafo">
-                    <h1 className="p-titulo">NTT DATA Europe & LATAM </h1>
-                </div>
-                
-                <div className="parrafo">
-                    <h1 className="p-titulo">NTT DATA Europe & LATAM </h1>
-                </div>
-                
-                <div className="parrafo">
-                    <h1 className="p-titulo">NTT DATA Europe & LATAM </h1>
-                </div>
-                
-                <div className="parrafo">
-                    <h1 className="p-titulo">NTT DATA Europe & LATAM </h1>
+                    <h4 className="p-fecha red">Octubre - 2022</h4>
+                    <h3 className="p-puesto">Java Talent Camp - Intermedio</h3>
+                    <h4 className="p-funciones">
+                        Curso de Java intermedio dictado por NTTDATA. <br /><br />
+                        <span className="red">•</span> Spring core <br /><br />
+                        <span className="red">•</span> Desarrollo e integración API REST con Spring<br /><br />
+                        <span className="red">•</span>  Spring Security <br /><br />
+                        <span className="red">•</span>  Springboot
+                    </h4>
                 </div>
 
+                <br /><hr className="linea"/>
+
+                <div className="parrafo">
+                    <h4 className="p-fecha red">Agosto - 2022</h4>
+                    <h3 className="p-puesto">TODOCODEACADEMY.COM</h3>
+                    <h4 className="p-funciones">
+                        Curso de Desarrollo de APIs en JAVA con SpringBoot. <br /><br />
+                    </h4>
+                </div>
+
+                <br /><hr className="linea"/>
+
+                <div className="parrafo">
+                    <h4 className="p-fecha red">Mayo - 2022</h4>
+                    <h3 className="p-puesto">Java Talent Camp - Inicial</h3>
+                    <h4 className="p-funciones">
+                        Curso de Java inicial dictado por NTTDATA. <br /><br />
+                        <span className="red">•</span> Fundamentos de la programación <br /><br />
+                        <span className="red">•</span> POO<br /><br />
+                        <span className="red">•</span> Patrones de diseño<br /><br />
+                        <span className="red">•</span>  Springboot
+                    </h4>
+                </div>
+                <br /><hr className="linea"/>
+
+                <div className="parrafo">
+                    <h4 className="p-fecha red">Marzo - 2021</h4>
+                    <h3 className="p-puesto">CODO A CODO - FrontEnd</h3>
+                    <h4 className="p-funciones">
+                        Curso de Java intermedio dictado por NTTDATA. <br /><br />
+                        <span className="red">•</span> Fundamentos de la programación con JavaSript <br /><br />
+                        <span className="red">•</span> POO <br /><br />
+                        <span className="red">•</span> HTML, CSS, JavaScript <br /><br />
+                        <span className="red">•</span> React Js
+                    </h4>
+                </div>
+
+                <br /><hr className="linea"/>
+
+                <div className="parrafo">
+                    <h4 className="p-fecha red">Agosto - 2022</h4>
+                    <h3 className="p-puesto">Desarrollo de APIs en JAVA con SpringBoot</h3>
+                    <h4 className="p-funciones">
+                        Curso de Java intermedio dictado por NTTDATA. <br /><br />
+                        <span className="red">•</span> Spring core <br /><br />
+                        <span className="red">•</span> Desarrollo e integración API REST con Spring<br /><br />
+                        <span className="red">•</span>  Spring Security <br /><br />
+                        <span className="red">•</span>  Springboot
+                    </h4>
+                </div>
+
+                <br /><hr className="linea"/>
+
+                <div className="parrafo">
+                    <h4 className="p-fecha red">Octubre - 2022</h4>
+                    <h3 className="p-puesto">Java Talent Camp - Intermedio</h3>
+                    <h4 className="p-funciones">
+                        Curso de Java intermedio dictado por NTTDATA. <br /><br />
+                        <span className="red">•</span> Spring core <br /><br />
+                        <span className="red">•</span> Desarrollo e integración API REST con Spring<br /><br />
+                        <span className="red">•</span>  Spring Security <br /><br />
+                        <span className="red">•</span>  Springboot
+                    </h4>
+                </div>
             </div>
         </div>
     );
