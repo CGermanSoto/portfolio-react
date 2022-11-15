2c3979534cd64c1eb2899684915d075a8a0e981b
CGermanSoto <cgermansoto@gmail.com>
2022-11-15 20:43:40 -0300

    cambios transitorios

--- src/Components/pages/Proyects.js
+++ src/Components/pages/Proyects.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../../Styles/Proyects.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Proyects(){
     return (
@@ -8,32 +10,17 @@ function Proyects(){
             <div className="parrafoAboutMe">
                 <div class="parent">
                     <div class="div1">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo1.png')} /></a>
-                    </div>
-                    <div class="div2">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/contador-clics.png')} /></a> 
-                    </div>
-                    
-                    <div class="div3">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo2.png')} /></a>
-                    </div>
-                    <div class="div4">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo3.png')} /></a>
-                    </div>
-                    <div class="div5">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo1.png')} /></a>
-                    </div>
-                    <div class="div6">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo1.png')} /></a>
-                    </div>
-                    <div class="div7">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo1.png')} /></a>
-                    </div>
-                    <div class="div8">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo1.png')} /></a>
-                    </div>
-                    <div class="div9">
-                        <a href="https://github.com/CGermanSoto/testimonios-freecodecamp" target='_blank'><img className="img-proyect" src={require('../img/repo1.png')} /></a>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                          <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                              Some quick example text to build on the card title and make up the
+                              bulk of the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
                     </div>
                 </div>
             </div>
