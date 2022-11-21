215226895f05ead50ae977ef9d4d715118c7cedb
CGermanSoto <cgermansoto@gmail.com>
2022-11-20 23:10:20 -0300

    Ok form contact

--- src/Components/pages/Contact.js
+++ src/Components/pages/Contact.js
@@ -1,19 +1,50 @@
-import React from "react";
 import '../../Styles/Contact.css'
-
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact(){
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_9a51jit', 'template_brzxp9h', form.current, 'dbkv3AVmSTzPsIyJv')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        const inputName = document.getElementById('name');
+        inputName.value='';
+        const inputMail = document.getElementById('mail');
+        inputMail.value='';
+        const inputText = document.getElementById('text');
+        inputText.value='';
+    };
+  
     return (
-        <div className="form-container">
-            <div><h1 className="title-contact"><a href="#">Enviar<span className="red">!</span></a></h1></div>
-            <div className="form-contact">
-                <input className='input' type='text' placeholder="Nombre"/>
-                <input className='input' type='text' placeholder="Asunto"/>
-                <input className='input' type='text' placeholder="Email"/>
-                <textarea className='input textarea' placeholder="Mensaje"/>
-            </div>
+        <>
+        <h1 className="window-title p"> Contacta<span className="red mi">me!</span></h1>
+        <div className='form-container'>
+            <form ref={form} onSubmit={sendEmail} className="form-contact">
+                
+                <input type="submit" value="Enviar" className='button-input'/>
+                
+               <div>
+                    <input type="text" name="user_name" placeholder='Name'className='input' id='name'/>
+                    <input type="email" name="user_mail" placeholder='Email'className='input' id='mail'/>
+                    <textarea name="user_text" placeholder='Mensaje' className='input textarea' id='text'/>
+               </div>
+            </form>
         </div>
 
+        <Popup className='popUp'>
+            
+        </Popup>
+        </>
+
+    
     );
 }
 
--- src/Components/pages/Cv.js
+++ src/Components/pages/Cv.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '../../Styles/Cv.css';
+import imgCv from '../img/abajo.png';
+import Documento from '../../downloads/CSotoCV.pdf';
 
 
 function Cv(){
@@ -8,15 +10,12 @@ function Cv(){
         <h1 className="window-title p"> <span className="red mi">Mi</span> Curriculum Vitae</h1>
         <div className="parrafoCv">
             <div className="parrafo">
-                <h1 className="p-titulo"><u>Descargar PDF</u></h1>
-                
+            <h1 className="p-titulo">Descargar CV.pdf</h1>
+                <a href={Documento} download>
+                    <img src={imgCv} className='imgCv'></img>
+                </a>
             </div>
 
-            <div className="parrafo">
-                <h1 className="p-titulo"><u>Ver online</u></h1>
-                
-            </div>
-            
         </div>
     </div>
     );
